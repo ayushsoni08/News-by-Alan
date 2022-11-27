6d5cef4529c4b5c11be46901ea4e7548b4a988bc
Ayush <ayushsoni0718@gmail.com>
2022-11-27 19:26:03 +0530

    Updates

---
+++ precache-manifest.808ecfd472960992c08b9a147e982257.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d74892b6f798111209c",
+    "url": "/News-by-Alan/static/css/main.06e708d0.chunk.css"
+  },
+  {
+    "revision": "7d74892b6f798111209c",
+    "url": "/News-by-Alan/static/js/main.0387acd3.chunk.js"
+  },
+  {
+    "revision": "c9b00b2ac0878139bc32",
+    "url": "/News-by-Alan/static/js/runtime~main.4851fba9.js"
+  },
+  {
+    "revision": "7aeaf4444a26c230af11",
+    "url": "/News-by-Alan/static/js/2.45cd9da0.chunk.js"
+  },
+  {
+    "revision": "722439febaa0c558b7b3d1688563e74c",
+    "url": "/News-by-Alan/index.html"
+  }
+];
